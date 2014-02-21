9c55b5f14254e91a97602ef5a93b92353a33a7e4
g3gg0 <unknown>
2014-02-21 20:31:45 +0100

    cleaned up NotifyBox functions, added new ones for different use cases

--- src/notify_box.c
+++ src/notify_box.c
@@ -26,7 +26,8 @@ static void notify_box_task(void* priv)
     
     TASK_LOOP
     {
-        notify_job_t *job = NULL;
+        notify_job_t *job_msg = NULL;
+        notify_job_t job;
 
         /* fetch a new encryption job */
         if(msg_queue_receive(notify_box_queue, &job, timeout))
@@ -34,48 +35,64 @@ static void notify_box_task(void* priv)
             continue;
         }
         
+        /* free job, it was allocated from the calling thread. we are keeping it local */
+        job = *job_msg;
+        free(job_msg);
+        
+        uint32_t border_width = 10;
         uint32_t font_height = 50;
-        uint32_t msg_width = bmp_string_width(FONT_CANON, job->message);
+        uint32_t msg_width = bmp_string_width(FONT_CANON, job.message);
         uint32_t msg_height = font_height;
-        uint32_t width = 20 + msg_width;
-        uint32_t height = 20 + msg_height;
+        uint32_t width = 2 * border_width + msg_width;
+        uint32_t height = 2 * border_width + msg_height;
         
         /* when there was an icon filename specified, open the bitmap */
-        if(job->icon_file)
+        if(job.icon_file)
         {
-            job->icon_data = bmp_load(job->icon_file, 0);
+            job.icon_data = bmp_load(job.icon_file, 0);
         }
         
         /* when the icon was successfully opened or passed, render it */
-        if(job->icon_data)
+        if(job.icon_data)
         {
-            width += job->icon_data->width + 10;
-            height = MAX(job->icon_data->height + 20, msg_height + 20);
+            if(!job.icon_width || !job.icon_height)
+            {
+                job.icon_width = job.icon_data->width;
+                job.icon_height = job.icon_data->height;
+            }
+            
+            /* if there is an icon, the space between icon and text is also 'border_width' */
+            width += job.icon_width + border_width;
+            height = MAX(job.icon_height + 2 * border_width, msg_height + 2 * border_width);
+        }
+        
+        /* default position is top left */
+        if(job.pos_x < 0 || job.pos_y < 0)
+        {
+            job.pos_x = 10;
+            job.pos_y = 10;
         }
         
         /* ToDo: detect number of lines and increase size */
         
-        
-        uint32_t pos_y = (480 - height) / 2;
-        uint32_t pos_x = (720 - width) / 2;
-        
         /* make sure we reach zero */
-        job->timeout -= job->timeout % delay;
+        job.timeout -= job.timeout % delay;
         
-        while(job->timeout > 0)
+        while(job.timeout > 0)
         {
-            job->timeout -= delay;
+            job.timeout -= delay;
             
-            bmp_fill(COLOR_BG, pos_x, pos_y, width, height);
+            bmp_fill(COLOR_BG, job.pos_x, job.pos_y, width, height);
+            bmp_draw_rect(COLOR_WHITE, job.pos_x, job.pos_y, width, height);
 
-            if(job->icon_data)
+            if(job.icon_data)
             {
-                bmp_draw_scaled_ex(job->icon_data, pos_x + 10, pos_y + 10, 128, 128, 0);
-                bmp_printf(FONT_CANON, pos_x + 128 + 20, pos_y + 10 + job->icon_data->height / 2 - msg_height / 2, job->message);
+                bmp_draw_scaled_ex(job.icon_data, job.pos_x + border_width, job.pos_y + border_width, job.icon_width, job.icon_height, 0);
+                bmp_printf(FONT_CANON, job.pos_x + job.icon_width + 2 * border_width, job.pos_y + border_width + job.icon_height / 2 - msg_height / 2, job.message);
             }
             else
             {
-                bmp_printf(FONT_CANON, pos_x + 10, pos_y + 10, job->message);
+                bmp_printf(FONT_CANON, job.pos_x + border_width, job.pos_y + border_width, job.message);
             }
             
             /* already another message arrived? if so, cancel loop */
@@ -91,28 +108,25 @@ static void notify_box_task(void* priv)
         }
         
         /* clean up the areas we printed on */
-        bmp_fill(COLOR_EMPTY, pos_x, pos_y, width, height);
+        bmp_fill(COLOR_EMPTY, job.pos_x, job.pos_y, width, height);
         redraw();
         
         /* free all elements in the job */
-        if(job->icon_file)
+        if(job.icon_file)
         {
-            free(job->icon_file);
+            free(job.icon_file);
             
             /* dont free icon_data if it was not allocated by us, so put it under filename check */
-            if(job->icon_data)
+            if(job.icon_data)
             {
-                free(job->icon_data);
+                free(job.icon_data);
             }
         }
         
-        if(job->message)
+        if(job.message)
         {
-            free(job->message);
+            free(job.message);
         }
-        
-        /* free job, it was allocated from the calling thread */
-        free(job);
     }
 }
 
@@ -136,60 +150,105 @@ void NotifyBoxHide()
     msg_queue_post(notify_box_queue, job);
 }
 
-void NotifyBox(uint32_t timeout, char *fmt, ...) 
+static void notify_box_process(notify_job_t job_data, char *fmt, va_list ap)
 {
     char *tmp_msg = malloc(NOTIFY_BOX_TEXT_LENGTH);
     
-    va_list ap;
-    va_start(ap, fmt);
-    vsnprintf(tmp_msg, NOTIFY_BOX_TEXT_LENGTH - 1, fmt, ap);
-    va_end(ap);
-    
-    /* displaying happens in other thread, so send a mesage with all information */
-    notify_job_t *job = malloc(sizeof(notify_job_t));
-    if(!job)
+    if(!tmp_msg)
     {
         return;
     }
     
-    /* fill the job for notify_box_task */
-    job->timeout = timeout;
+    notify_job_t *job = malloc(sizeof(notify_job_t));
+    if(!job)
+    {
+        free(tmp_msg);
+        return;
+    }
+    
+    *job = job_data;
+    vsnprintf(tmp_msg, NOTIFY_BOX_TEXT_LENGTH - 1, fmt, ap);
     job->message = strdup(tmp_msg);
-    job->icon_file = NULL;
-    job->icon_data = NULL;
+    
+    free(tmp_msg);
     
     msg_queue_post(notify_box_queue, job);
+}
 
-    /* this buffer was only used for vsnprintf */
-    free(tmp_msg);
+void NotifyBox(uint32_t timeout, char *fmt, ...) 
+{
+    notify_job_t job;
+    
+    /* fill the job for notify_box_task */
+    job.timeout = timeout;
+    job.icon_file = NULL;
+    job.icon_data = NULL;
+    job.icon_width = 0;
+    job.icon_height = 0;
+    job.pos_x = -1;
+    job.pos_y = -1;
+    
+    va_list ap;
+    va_start(ap, fmt);
+    notify_box_process(job, fmt, ap);
+    va_end(ap);
 }
 
 void NotifyBoxIcon(uint32_t timeout, char *icon, char *fmt, ...) 
 {
-    char *tmp_msg = malloc(NOTIFY_BOX_TEXT_LENGTH);
+    notify_job_t job;
+    
+    /* fill the job for notify_box_task */
+    job.timeout = timeout;
+    job.icon_file = strdup(icon);
+    job.icon_data = NULL;
+    job.icon_width = 0;
+    job.icon_height = 0;
+    job.pos_x = -1;
+    job.pos_y = -1;
     
     va_list ap;
     va_start(ap, fmt);
-    vsnprintf(tmp_msg, NOTIFY_BOX_TEXT_LENGTH - 1, fmt, ap);
+    notify_box_process(job, fmt, ap);
     va_end(ap);
-    
-    /* displaying happens in other thread, so send a mesage with all information */
-    notify_job_t *job = malloc(sizeof(notify_job_t));
-    if(!job)
-    {
-        return;
-    }
+}
+
+void NotifyBoxIconFileAdv(uint32_t timeout, char *icon, int32_t icon_width, uint32_t icon_height, uint32_t pos_x, uint32_t pos_y, char *fmt, ...) 
+{
+    notify_job_t job;
     
     /* fill the job for notify_box_task */
-    job->timeout = timeout;
-    job->message = strdup(tmp_msg);
-    job->icon_file = strdup(icon);
-    job->icon_data = NULL;
+    job.timeout = timeout;
+    job.icon_file = strdup(icon);
+    job.icon_data = NULL;
+    job.icon_width = 0;
+    job.icon_height = 0;
+    job.pos_x = -1;
+    job.pos_y = -1;
     
-    msg_queue_post(notify_box_queue, job);
+    va_list ap;
+    va_start(ap, fmt);
+    notify_box_process(job, fmt, ap);
+    va_end(ap);
+}
 
-    /* this buffer was only used for vsnprintf */
-    free(tmp_msg);
+void NotifyBoxIconDataAdv(uint32_t timeout, struct bmp_file_t *icon, int32_t icon_width, uint32_t icon_height, uint32_t pos_x, uint32_t pos_y, char *fmt, ...) 
+{
+    notify_job_t job;
+    
+    /* fill the job for notify_box_task */
+    job.timeout = timeout;
+    job.icon_file = NULL;
+    job.icon_data = icon;
+    job.icon_width = icon_width;
+    job.icon_height = icon_height;
+    job.pos_x = pos_x;
+    job.pos_y = pos_y;
+    
+    va_list ap;
+    va_start(ap, fmt);
+    notify_box_process(job, fmt, ap);
+    va_end(ap);
 }
 
 static void dlg_init()
--- src/notify_box.h
+++ src/notify_box.h
@@ -6,13 +6,22 @@
 typedef struct
 {
     uint32_t timeout;
+    /* position where to print the dialog */
+    int32_t pos_x;
+    int32_t pos_y;
     char *message;
+    /* if specified, the icon is scaled */
+    uint32_t icon_width;
+    uint32_t icon_height;
+    /* if there is an icon filename specified, we will open it and load+free the bitmap data */
     char *icon_file;
+    /* if there is only bitmap data specified, it won't get free'd */
     struct bmp_file_t *icon_data;
 } notify_job_t;
 
 void NotifyBoxHide();
 void NotifyBox(uint32_t timeout, char* fmt, ...) ;
-void NotifyBoxIcon(uint32_t timeout, char *icon, char* fmt, ...);
-
+void NotifyBoxIcon(uint32_t timeout, char *icon, char *fmt, ...);
+void NotifyBoxIconFileAdv(uint32_t timeout, char *icon, int32_t icon_width, uint32_t icon_height, uint32_t pos_x, uint32_t pos_y, char *fmt, ...);
+void NotifyBoxIconDataAdv(uint32_t timeout, struct bmp_file_t *icon, int32_t icon_width, uint32_t icon_height, uint32_t pos_x, uint32_t pos_y, char *fmt, ...);
 #endif
