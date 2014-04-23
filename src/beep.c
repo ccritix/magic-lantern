87e9e4a5b0454f4dccca062055ded05f7cb35ba2
Maqs <marcus@eunomia.de>
2014-04-23 18:52:54 +0200

    Make beep.c compile again in case CONFIG_BEEP is not defined.

--- src/beep.c
+++ src/beep.c
@@ -314,11 +314,11 @@ TASK_CREATE("beep_task", beep_task, 0, 0x18, 0x1000 );
 
 void unsafe_beep(){}
 void beep(){}
-void beep_times(int times){};
+void beep_times(uint32_t times){};
 int beep_enabled = 0;
 void WAV_StartRecord(char* filename){};
 void WAV_StopRecord(){};
-void beep_custom(int duration, int frequency, int wait) {}
+void beep_custom(uint32_t duration, uint32_t frequency, uint32_t wait) {}
 
 #endif
 
