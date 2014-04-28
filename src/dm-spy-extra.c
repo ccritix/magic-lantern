5f8afcb599de385827a96b6fd590ae505f700b5d
a1ex <unknown>
2014-04-28 11:23:57 +0300

    dm-spy-extra: updated check for cache conflicts (found one more conflict, also detected by the patch manager)

--- src/dm-spy-extra.c
+++ src/dm-spy-extra.c
@@ -44,7 +44,7 @@ static struct logged_func logged_functions[] = {
     { 0xff9b3cb4, "register_interrupt", 4 },
     { 0xffb277c8, "register_obinteg_cbr", 2 },
     { 0xffaf6930, "set_digital_gain_and_related", 3 },
-    { 0xffaf68a4, "set_saturate_offset", 1 },
+    //~ { 0xffaf68a4, "set_saturate_offset", 1 },
     { 0xffaf686c, "set_saturate_offset_2", 1 },
     { 0xff987200, "set_fps_maybe", 1 },
     { 0xffa38114, "set_fps_maybe_2", 1 },
@@ -163,6 +163,55 @@ static void state_transition_log(breakpoint_t *bkpt)
     );
 }
 
+static int check_no_conflicts(int i)
+{
+    int index_mask = CACHE_INDEX_ADDRMASK(TYPE_ICACHE);
+    
+    for (int j = 0; j < i; j++)
+    {
+        /* A GDB watchpoint uses two breakpoints, at addr and addr+4, and only one of them is active (armed). */
+        /* Therefore, it's not enough to call cache_is_patchable, since it may still conflict with inactive breakpoints. */
+        /* so, we inspect the addresses manually for now. */
+        /* TODO: this check should probably be moved in GDB hooks or in the patch library */
+        
+        int ai = logged_functions[i].addr;     /* address of the main breakpoint in the GDB watchpoint */
+        int aj = logged_functions[j].addr;
+        int bi = logged_functions[i].addr + 4; /* address of the second breakpoint */
+        int bj = logged_functions[j].addr + 4;
+        if (((ai & index_mask) == (aj & index_mask)) ||
+            ((bi & index_mask) == (bj & index_mask)) ||
+            ((ai & index_mask) == (bj & index_mask)) ||
+            ((bi & index_mask) == (aj & index_mask)))
+        {
+            console_show();
+            printf("Birthday paradox: %x %s\n"
+                   "  conflicts with: %x %s.\n", 
+                logged_functions[i].addr, logged_functions[i].name,
+                logged_functions[j].addr, logged_functions[j].name
+            );
+            return 0;
+        }
+    }
+    
+    /* all fine; let's also check the other cache patches, unrelated to our hooks */
+    if (!cache_locked())
+    {
+        /* no conflicts possible */
+        return 1;
+    }
+    if ((cache_is_patchable(logged_functions[i].addr,   TYPE_ICACHE) && 
+         cache_is_patchable(logged_functions[i].addr+4, TYPE_ICACHE)))
+    {
+        /* seems OK */
+        return 1;
+    }
+
+    /* NOK, conflict with some other patch */
+    console_show();
+    printf("%x %s not patchable.\n", logged_functions[i].addr, logged_functions[i].name);
+    return 0;
+}
+
 void dm_spy_extra_install()
 {
     gdb_setup();
@@ -179,35 +228,12 @@ void dm_spy_extra_install()
     {
         if (logged_functions[i].addr)
         {
-            if (cache_is_patchable(logged_functions[i].addr,   TYPE_ICACHE) && 
-                cache_is_patchable(logged_functions[i].addr+4, TYPE_ICACHE))
+            if (check_no_conflicts(i))
             {
                 logged_functions[i].bkpt = gdb_add_watchpoint(
                     logged_functions[i].addr, 0,
                     logged_functions[i].log_func ? logged_functions[i].log_func : generic_log
                 );
-            }
-            else
-            {
-                console_show();
-                int index_mask = CACHE_INDEX_ADDRMASK(TYPE_ICACHE);
-                int found = 0;
-                for (int j = 0; j < i; j++)
-                {
-                    if ((logged_functions[i].addr & index_mask) == (logged_functions[j].addr & index_mask))
-                    {
-                        printf("Birthday paradox: %x %s\n"
-                               "  conflicts with: %x %s.\n", 
-                            logged_functions[i].addr, logged_functions[i].name,
-                            logged_functions[j].addr, logged_functions[j].name
-                        );
-                        found = 1;
-                    }
-                }
-                if (!found)
-                {
-                    printf("%x %s not patcheable.\n", logged_functions[i].addr, logged_functions[i].name);
-                }
             }
         }
     }
