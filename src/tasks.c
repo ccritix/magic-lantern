aae0cd22fd1b792bf51bd5ad01f29fb66da3125f
a1ex <unknown>
2014-08-21 13:58:29 +0300

    get_current_task_name: identify interrupt IDs (5D2/5D3/60D, requires finding a constant)

--- src/tasks.c
+++ src/tasks.c
@@ -59,7 +59,17 @@ char* get_current_task_name()
     }
     else
     {
+    #ifdef CURRENT_INTERRUPT_ADDR
+        static char isr[] = "**INT-00h**";
+        int i = MEM(CURRENT_INTERRUPT_ADDR) >> 2;
+        int i0 = (i & 0xF);
+        int i1 = (i >> 4) & 0xF;
+        isr[6] = i1 < 10 ? '0' + i1 : 'A' + i1 - 10;
+        isr[7] = i0 < 10 ? '0' + i0 : 'A' + i0 - 10;
+        return isr;
+    #else
         return "**INTERRUPT**";
+    #endif
     }
 }
 
