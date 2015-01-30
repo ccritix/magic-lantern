d4805529690c2627904b1f17364cd438a30193a0
David Milligan <david.l.milligan@gmail.com>
2015-01-29 20:17:30 -0500

    mlv_dump: handle metadata correctly for different bit depths (image data is wrong though if saving as DNG with different bpp)

--- modules/mlv_rec/mlv_dump.c
+++ modules/mlv_rec/mlv_dump.c
@@ -2229,12 +2229,26 @@ read_headers:
                             raw_info = lv_rec_footer.raw_info;
                             raw_info.frame_size = frame_size;
                             raw_info.buffer = frame_buffer;
+                            if(new_depth)
+                            {
+                                raw_info.bits_per_pixel = new_depth;
+                                if(old_depth > new_depth)
+                                {
+                                    raw_info.black_level >>= old_depth - new_depth;
+                                    raw_info.white_level >>= old_depth - new_depth;
+                                }
+                                else
+                                {
+                                    raw_info.black_level <<= new_depth - old_depth;
+                                    raw_info.white_level <<= new_depth - old_depth;
+                                }
+                            }
 
                             /* override the resolution from raw_info with the one from lv_rec_footer, if they don't match */
                             if (lv_rec_footer.xRes != raw_info.width)
                             {
                                 raw_info.width = lv_rec_footer.xRes;
-                                raw_info.pitch = raw_info.width * 14/8;
+                                raw_info.pitch = raw_info.width * raw_info.bits_per_pixel/8;
                                 raw_info.active_area.x1 = 0;
                                 raw_info.active_area.x2 = raw_info.width;
                                 raw_info.jpeg.x = 0;
