840b4bb6fddbb9ec4df4e4508d47d7f4db0ddc2f
Unknown <alex@thinkpad>
2016-05-08 11:18:03 +0300

    60D: load autoexec.bin in AllocateMemory pool (640K reserved)

--- platform/60D.111/internals.h
+++ platform/60D.111/internals.h
@@ -101,6 +101,9 @@
 /** We can display some extra info in photo mode (not LiveView) **/
 #define CONFIG_PHOTO_MODE_INFO_DISPLAY
 
+/** This camera loads ML into the AllocateMemory pool **/
+#define CONFIG_ALLOCATE_MEMORY_POOL
+
 /** FIO_RenameFile works **/
 #define CONFIG_FIO_RENAMEFILE_WORKS
 
