7b102375691f7eb09b0dfe23c7ccf8a96854467e
Unknown <alex@thinkpad>
2016-05-15 21:23:58 +0300

    Enabled CONFIG_LVAPP_HACK_PATCH for 5D3 1.1.3

--- platform/5D3.113/internals.h
+++ platform/5D3.113/internals.h
@@ -152,5 +152,5 @@
 /** this method bypasses Canon's lv_save_raw and slurps the raw data directly from connection #0 */
 #define CONFIG_EDMAC_RAW_SLURP
 
-/** Hide Canon bottom bar from DebugMsg hook */
-#define CONFIG_LVAPP_HACK_DEBUGMSG
+/** Patch LiveViewApp dialog hander to hide Canon bottom bar */
+#define CONFIG_LVAPP_HACK_PATCH
