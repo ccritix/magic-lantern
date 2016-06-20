75deb9bd52f57b2b36872f423aea365e90b22e99
Unknown <alex@thinkpad>
2016-06-20 09:40:39 +0300

    760D: attempt to guess the LED address

--- platform/760D.100/reboot.c
+++ platform/760D.100/reboot.c
@@ -16,6 +16,39 @@ asm(
     "B       cstart\n"
 );
 
+static void busy_wait(int n)
+{
+    int i,j;
+    static volatile int k = 0;
+    for (i = 0; i < n; i++)
+        for (j = 0; j < 100000; j++)
+            k++;
+}
+
+static void blink(int n)
+{
+    /* https://chdk.setepontos.com/index.php?topic=11316.msg111290#msg111290
+     * https://chdk.setepontos.com/index.php?topic=1493.msg13469#msg13469
+     * 7D2 LED is at 0xd20b0c34 (i=269).
+     */
+    while (1)
+    {
+        for (int i = 0; i < 512; i++)
+        {
+            *(volatile int*)(0xd20b0800 + i * 4) = 0x4c0003;
+        }
+
+        busy_wait(n);
+
+        for (int i = 0; i < 512; i++)
+        {
+            *(volatile int*)(0xd20b0800 + i * 4) = 0x4d0002;
+        }
+
+        busy_wait(n);
+    }
+}
+
 /* read/write a uint32_t from memory */
 #define MEM(x) *(volatile uint32_t*)(x)
 
@@ -24,6 +57,9 @@ __attribute__((noreturn))
 __attribute__ ((section(".dump_asm")))
 cstart( void )
 {
+    /* uncomment this to try blinking the LED */
+    //~ blink(10);
+    
     // look for main firmware start
     // 7D2: add r0, pc, #4; orr r0, r0, #1; bx r0
     for (uint32_t start = 0xF8000000; start < 0xFFFF0000; start += 4)
