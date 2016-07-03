8e4ecee3c9d9ba0ba7747f5cb69715fd51fae738
Unknown <alex@thinkpad>
2016-07-03 15:29:13 +0300

    760D/80D: attempt to identify the LED address (thanks zloe)

--- platform/760D.100/reboot.c
+++ platform/760D.100/reboot.c
@@ -49,17 +49,72 @@ static void blink(int n)
     }
 }
 
+/* Attempt to identify the LED address, knowing it is somewhere
+ * between 0xd20b0800 and 0xd20b1000 (512 possible MMIO addresses).
+ * 
+ * On each address, we will send its binary representation (9 bits),
+ * all in parallel; I expect only one of those 512 addresses (the LED)
+ * will actually blink, so you would just write down the blink sequence
+ * (long/short) and identify the LED address.
+ *
+ * n ... time for short blinks, total cycle time = 6 n,
+ * long blinks are 3 n long and pause between blinks is 3 n
+ * between two cycles of address blinks we have an additional pause of 4 n
+ * 
+ * credits: zloe
+ */
+static void guess_led(int n)
+{
+    const int N = 512;
+    
+    while (1)
+    {
+        for (int bit = 8; bit >= 0; --bit)
+        {
+            // turn all led on all addresses on
+            for (int i = 0; i < N; ++i)
+            {
+                *(volatile int*)(0xd20b0800 + i * 4) = 0x4c0003;
+            }
+            
+            // sleep short
+            busy_wait(n);
+            
+            // turn off all addresses which have 0 on the currently selected bit
+            for (int i = 0; i < N; ++i)
+            {
+                int shortBlink = (i & (1<<bit)) == 0;
+                if (shortBlink)
+                {
+                    *(volatile int*)(0xd20b0800 + i * 4) = 0x4d0002;
+                }
+            }
+
+            // additional sleep for the long time period
+            // 2 n to more easily distinguish short and long blinks
+            busy_wait(2*n);
+
+            // turn led off on all addresses
+            for (int i = 0; i < N; ++i)
+            {
+                *(volatile int*)(0xd20b0800 + i * 4) = 0x4d0002;
+            }
+            
+            // pause between blinking
+            busy_wait(3*n);
+        }
+        
+        // pause before we repeat the code
+        busy_wait(4*n);
+    }
+}
+
 /* read/write a uint32_t from memory */
 #define MEM(x) *(volatile uint32_t*)(x)
 
-void
-__attribute__((noreturn))
-__attribute__ ((section(".dump_asm")))
-cstart( void )
+
+static void try_jumping_to_firmware()
 {
-    /* uncomment this to try blinking the LED */
-    //~ blink(10);
-    
     // look for main firmware start
     // 7D2: add r0, pc, #4; orr r0, r0, #1; bx r0
     for (uint32_t start = 0xF8000000; start < 0xFFFF0000; start += 4)
@@ -75,6 +130,18 @@ cstart( void )
             while(1);
         }
     }
+}
+
+void
+__attribute__((noreturn))
+__attribute__ ((section(".dump_asm")))
+cstart( void )
+{
+    /* Try guessing the LED address */
+    guess_led(10);
+    
+    /* Try jumping to main firmware from FIR */
+    //~ try_jumping_to_firmware();
     
     // should be unreachable
     while(1);
