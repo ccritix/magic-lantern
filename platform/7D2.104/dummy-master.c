f58edfbf1eef89b8b7a6051175d367d892be76a0
Unknown <alex@thinkpad>
2016-07-16 14:06:49 +0300

    7D2: fix boot on the second digic

--- platform/7D2.104/dummy-master.c
+++ platform/7D2.104/dummy-master.c
@@ -5,5 +5,8 @@ asm(
 
     /* jump to Canon firmware */
     /* note: with B 0xFE0A0000, gcc inserts a veneer */
+    "LDR R0, =0xD20C0084\n"
+    "MOV R1, #0\n"
+    "STR R1, [R0]\n"
     "LDR PC, =0xFE0A0000\n"
 );
