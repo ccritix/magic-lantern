900f34cffd615395f01d41424926ff0fcc5e8f78
Mathew Kelly <dkelly11@tampabay.rr.com>
2016-07-27 07:22:30 +0000

    1200D state-object SDS_FRONT3_STATE change

--- platform/1200D.101/include/platform/state-object.h
+++ platform/1200D.101/include/platform/state-object.h
@@ -5,6 +5,6 @@
 #define INPUT_ENABLE_IMAGE_PHYSICAL_SCREEN_PARAMETER 20
 #define EVF_STATE (*(struct state_object **)0x48EC)        // mk11174
 #define MOVREC_STATE (*(struct state_object **)0x5664)     // mk11174
-#define SDS_FRONT3_STATE (*(struct state_object **)0x3148) // mk11174
+#define SDS_FRONT3_STATE (*(struct state_object **)0x31CC) // mk11174 still not 100%, maybe alex can confirm?
 
 #endif // __platform_state_object_h
