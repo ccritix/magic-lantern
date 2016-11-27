71b9813561a2616a101d48076b30e6788e1b3d93
David Milligan <david.l.milligan@gmail.com>
2016-11-26 21:45:35 -0500

    xmp.lua: code comments and minor refactorings

--- scripts/lens.lua
+++ scripts/lens.lua
@@ -11,9 +11,11 @@ lenses =
     { name = "My Lens", focal_length = 50 },
 }
 
-selected_index = 1
 selector_instance = nil
-xmp_instance = nil
+
+xmp:add_property(xmp.lens_name, function() return lens.name end)
+xmp:add_property(xmp.focal_length, function() return lens.focal_length end)
+xmp:add_property(xmp.aperture, function() return math.floor(camera.aperture.value * 10) end)
 
 function property.LENS_NAME:handler(value)
     if lens.is_chipped == false then
@@ -22,9 +24,7 @@ function property.LENS_NAME:handler(value)
         if selector_instance ~= nil then
             selector_instance.cancel = true
         end
-        if xmp_instance ~= nil then
-            xmp_instance:disable()
-        end
+        xmp:stop()
     end
 end
 
@@ -33,17 +33,10 @@ function select_lens()
     display.rect(0, 0, display.width, display.height, COLOR.BLACK, COLOR.BLACK)
     selector_instance = selector.create("Select Manual Lens", lenses, function(l) return l.name end, 600)
     if selector_instance:select() then
-        selected_index = selector_instance.index
-        for k,v in pairs(lenses[selected_index]) do
+        for k,v in pairs(lenses[selector_instance.index]) do
             lens[k] = v
         end
-        if xmp_instance == nil then
-            xmp_instance = xmp.create()
-            xmp_instance:add_property(xmp.lens_name, function() return lens.name end)
-            xmp_instance:add_property(xmp.focal_length, function() return lens.focal_length end)
-            xmp_instance:add_property(xmp.aperture, function() return math.floor(camera.aperture.value * 10) end)
-        end
-        xmp_instance:enable()
+        xmp:start()
     end
     selector_instance = nil
     menu.close()
--- scripts/lib/xmp.lua
+++ scripts/lib/xmp.lua
@@ -1,11 +1,16 @@
 --[[---------------------------------------------------------------------------
-Functions for writing supplemental xmp metadata
+Helper for writing supplemental xmp metadata
+
+This module is a lua script (@{xmp.lua}), you need to explicitly load it
+with `require('xmp')`
+
+@module xmp
 ]]
 
 GUISTATE_QR = 3
 
 xmp = {}
-xmp.__index = xmp
+xmp.properties = {}
 
 xmp.template = [[
 <x:xmpmeta xmlns:x="adobe:ns:meta/" x:xmptk="Adobe XMP Core 5.6-c011 79.156380, 2014/05/21-23:38:37        ">
@@ -24,6 +29,7 @@ xmp.property_format = [[
     %s="%s"
 ]]
 
+-- predefined xmp properties
 xmp.lens_name = { name = "aux:Lens" }
 xmp.focal_length = { name = "exif:FocalLength", format = "%d/1"}
 xmp.aperture = { name = "exif:FNumber", format = "%d/10" }
@@ -32,15 +38,11 @@ function xmp.get_sidecar_filename()
     return string.format("DCIM/%03dCANON/%s%04d.XMP", dryos.shooting_card.folder_number, dryos.prefix, dryos.shooting_card.file_number)
 end
 
-function xmp.create()
-    local x = {}
-    setmetatable(x, xmp)
-    x.properties = {}
-    x:enable()
-    return x
-end
-
-function xmp:enable()
+--[[---------------------------------------------------------------------------
+Starts listening for pictures being taken to write xmp metadata.
+@function start
+]]
+function xmp:start()
     local this = self
     function property.GUI_STATE:handler(value)
         if value == GUISTATE_QR then
@@ -49,10 +51,20 @@ function xmp:enable()
     end
 end
 
-function xmp:disable()
+--[[---------------------------------------------------------------------------
+Stops listening for pictures being taken.
+@function start
+]]
+function xmp:stop()
     property.GUI_STATE.handler = nil
 end
 
+--[[---------------------------------------------------------------------------
+Adds a new metadata property to record in the xmp file
+@param property the property to add, should be a table with a name and optionally a string format
+@param value the value of the property or a function that returns the value
+@function add_property
+]]
 function xmp:add_property(property, value)
     if property.format ~= nil then
         if type(value) == "function" then
@@ -67,6 +79,11 @@ function xmp:add_property(property, value)
     end
 end
 
+--[[---------------------------------------------------------------------------
+Writes the xmp metdata to file
+@param filename optional - the filename to write to, leave blank to use the sidecar filename for the most recently taken photo
+@function write
+]]
 function xmp:write(filename)
     local f = io.open(filename or self.get_sidecar_filename(), "w+")
     local str = ""
