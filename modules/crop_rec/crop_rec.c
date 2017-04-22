54e361ab6f4d7b75650c2804c33abbbdbe951f77
Unknown <alex@thinkpad>
2017-04-22 21:56:59 +0300

    crop_rec: fix shutter speed in centered x5 zoom

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -689,6 +689,13 @@ static void FAST adtg_hook(uint32_t* regs, uint32_t* stack, uint32_t pc)
         return;
     }
 
+    if (crop_preset == CROP_PRESET_CENTER_Z)
+    {
+        /* no ADTG overrides required */
+        /* fixme: they will actually interfere with shutter speed */
+        return;
+    }
+
     if (is_5D3 && !is_720p())
     {
         if (crop_preset == CROP_PRESET_3x3_1X ||
