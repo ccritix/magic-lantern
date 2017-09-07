a1db80ab9dae370d0c695b5699ac5a693811e0fc
Unknown <alex@thinkpad>
2017-09-07 15:25:18 +0300

    1200D: attempt to fix half-shutter button CFn (not tested)

--- platform/1200D.101/cfn.c
+++ platform/1200D.101/cfn.c
@@ -11,7 +11,8 @@ void set_htp(int value) { SetCFnData(0, 5, value); }
 int get_mlu() { return 0; }
 void set_mlu(int value) { return; }
 
-int cfn_get_af_button_assignment() { return GetCFnData(0, 7); }
-void cfn_set_af_button(int value) { SetCFnData(0, 7, value); }
+/* unsure - from https://www.manualslib.com/manual/1147171/Canon-Eos-1200d.html?page=257 */
+int cfn_get_af_button_assignment() { return GetCFnData(0, 8); }
+void cfn_set_af_button(int value) { SetCFnData(0, 8, value); }
 
 GENERIC_GET_ALO
