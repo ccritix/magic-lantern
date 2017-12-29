91115df7d7787116fff7e8df0a79a81f87c53df4
Alessandro Profiti <aprofiti@users.noreply.github.com>
2017-12-29 04:12:00 +0100

    lens.c: Increased "Lens Info Prefs"  menu width to 700

--- src/lens.c
+++ src/lens.c
@@ -1896,6 +1896,7 @@ static struct menu_entry tweak_menus[] = {
    {
         .name = "Lens Info Prefs",
         .select   = menu_open_submenu,
+	.submenu_width = 700,
         .children =  (struct menu_entry[]) {
             #ifndef CONFIG_FULLFRAME
             {
