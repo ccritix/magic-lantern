088efeb2aba04b8f1f1e351162728cd9602764a1
Stefan Horst <stefan@ultrachaos.de>
2018-01-16 15:58:26 +0100

    fixed EVF_STATE; now only the prop_mgr assert is missing; with small set of features;

--- platform/1300D.110/include/platform/state-object.h
+++ platform/1300D.110/include/platform/state-object.h
@@ -3,7 +3,7 @@
 
 //#define DISPLAY_STATE DISPLAY_STATEOBJ
 #define INPUT_ENABLE_IMAGE_PHYSICAL_SCREEN_PARAMETER 20
-#define EVF_STATE (*(struct state_object **)0x3737C)
+#define EVF_STATE (*(struct state_object **)0x37930)
 #define MOVREC_STATE (*(struct state_object **)0x38744)
 
 #endif // __platform_state_object_h
