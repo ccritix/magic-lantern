9400bd72f8c44eb68d35c2b99dabb10058adbb73
danne <dudek53@gmail.com>
2018-06-16 14:22:17 +0200

    2904x1248(100D)

--- modules/adtg_gui/adtg_gui.c
+++ modules/adtg_gui/adtg_gui.c
@@ -861,37 +861,43 @@ static int res3k_reg(int reg)
     if (is_camera("100D", "1.0.1"))
     {
 
+       if (regs[reg].dst == DST_CMOS)
+       {
+           switch (regs[reg].reg)
+           {
+		 case 5:
+	  	    return 0x280;       /* CMOS[5]: ISO related? */
+                 case 7:
+		    return 0xa89;       /* CMOS[7]: ISO related? */
+           }
+       }
+
        if (regs[reg].dst == 0xC0F0)
        {
 
            switch (regs[reg].reg)
            {
-                case 0x6800:                /* C0F06804 - raw resolution */
-                    return 0x40019;
                 case 0x6804:                /* C0F06804 - raw resolution */
-                    return 0x50802a1;       /* Valid liveview 2520x1248 24fps 14-bit lossless */
+                    return 0x5080301;       /* 2904x1248 24fps 14-bit lossless */
                  /* return 0x5080301;       2904x1248 24fps crushed liveview 14-bit lossless from 0x528011B -> 3072px in raw_rec */ 
-                case 0x6824:
-                   return 0x4ca;
-                case 0x6828:
-                   return 0x4ca;
-                case 0x682c:
-                   return 0x4ca;
-                case 0x6830:
-                   return 0x4ca;
-                case 0x6008:
-                   return 0x35b035b;
-                case 0x600c:
-                   return 0x35b035b;
-                case 0x6010:
-                   return 0x35b;
-                case 0x6014:
-                   return 0x60b;
-                case 0x7150:
-                  return 0x428;
-                case 0x713c:
-                   return 0x505;  
-      
+	    	case 0x6824:
+		    return 0x3ca;
+	    	case 0x6828:
+		    return 0x3ca;
+	    	case 0x682c:
+		    return 0x3ca;
+	    	case 0x6830:
+		    return 0x3ca;
+	    	case 0x6008:
+		    return 0x37b037b;
+	    	case 0x600c:
+		    return 0x37b037b;
+	    	case 0x6010: 	
+		    return 0x37b;
+	    	case 0x6014:
+		    return 0x6d7;
+	    	case 0x713c:
+		    return 0x555;
             }
 
         }
@@ -899,12 +905,12 @@ static int res3k_reg(int reg)
         { 
             switch (regs[reg].reg)
             {
-                 case 0x8172:
-                    return 0x437;
-                 case 0x8178:
-                    return 0x437;
-                 case 0x8179:
-                   return 0x587;
+            case 0x82b6:
+	       return 0x7f4;      /* it's 5 in zoom mode and 6 in 1080p; this also overrides ADTG4 */
+            case 0x8172:
+	       return 0x77c; /* without this, you get some weird artifacts; this should only go to ADTG2, not 4 */
+	    case 0x8178:
+	       return 0x77c;
 
             }
 
