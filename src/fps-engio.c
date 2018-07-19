edea40a93160641b08b8fac871a6cdbcebdd1bef
danne <dudek53@gmail.com>
2018-07-19 12:10:02 +0200

    fps override registers

--- src/fps-engio.c
+++ src/fps-engio.c
@@ -236,9 +236,8 @@ static void fps_read_current_timer_values();
     #define TG_FREQ_BASE 32000000
     #define FPS_TIMER_A_MIN (ZOOM ? 716 : MV1080CROP ? 532 : 520)
     #undef FPS_TIMER_B_MIN
-    #define FPS_TIMER_B_MIN ( \
-    RECORDING_H264 ? (MV1080CROP ? 1750 : MV720 ? 990 : 1970) \
-                   : (ZOOM || MV1080CROP ? 1288 : 1970))
+/* testing overriding fps. 38fps seems max */
+    #define FPS_TIMER_B_MIN (ZOOM || MV1080 || MV1080CROP ? 1288 : MV720 || (lv && lv_dispsize==1 && !is_movie_mode()) ? 990 : 1970)
 #elif defined(CONFIG_6D)
     #define TG_FREQ_BASE 25600000
     #define FPS_TIMER_A_MIN (fps_timer_a_orig - (ZOOM ? 22 : MV720 ? 10 : 34) ) //, ZOOM ? 708 : 512)
