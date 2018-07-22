ff6ed74fd7ebdaea19cb7fa1786dd4412f4caf53
danne <dudek53@gmail.com>
2018-07-22 17:36:17 +0200

    adding back lua scripts

---
+++ scripts/SDoverclock.lua
@@ -0,0 +1,32 @@
+-- enable SD overclocking
+
+  console.hide()
+  menu.close()
+if menu.get("Debug", "SD overclock", "MAY CAUSE DATA LOSS") == "MAY CAUSE DATA LOSS" then
+  if menu.get("Movie", "RAW video", "") == "OFF" then
+    display.notify_box("enabling of SD overclocking")
+    menu.set("Debug", "SD overclock", "ON")
+    msleep(1000)
+    display.notify_box("patching...")
+    msleep(2000)
+    display.notify_box("done!")
+    msleep(1000)
+  else
+    menu.set("Movie", "RAW video", "OFF")
+    display.notify_box("enabling of SD overclocking")
+    menu.set("Debug", "SD overclock", "ON")
+    msleep(1000)
+    display.notify_box("patching...")
+    msleep(2000)
+    menu.set("Movie", "RAW video", "ON")
+    display.notify_box("done!")
+    msleep(1000)
+  end
+else
+  display.notify_box("Please enable sd_uhs.mo")
+  msleep(2000)
+  display.notify_box("Please enable sd_uhs.mo")
+  msleep(2000)
+  return
+end
+
