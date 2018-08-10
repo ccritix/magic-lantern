c46f6389fd3cf8259340a95358a4d74aeb27480e
Unknown <alex@thinkpad>
2018-08-10 21:59:13 +0200

    M50: experimental startup code based on 80D's minimal.c (partially tested in QEMU)

--- src/fw-signature.h
+++ src/fw-signature.h
@@ -23,6 +23,7 @@
 #define SIG_650D_104 0x4B7FC4D0 // from FF0C0000
 #define SIG_700D_114 0x4b35ce13 // from FF0C0000
 #define SIG_200D_101 0xf72c729a // from E0040000
+#define SIG_M50_101  0x80bd9b4b // from E0040000
 
 static uint32_t compute_signature(uint32_t * start, uint32_t num)
 {
