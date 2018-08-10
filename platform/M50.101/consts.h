544f80d44f84ca45f0e4563e19785c1ec7666cad
Unknown <alex@thinkpad>
2018-08-10 10:13:51 +0200

    M50: LED address (not tested) https://www.magiclantern.fm/forum/index.php?topic=19737.msg204984#msg204984

--- platform/M50.101/consts.h
+++ platform/M50.101/consts.h
@@ -1,3 +1,7 @@
 /*
  *  EOS M50 1.0.1 consts
  */
+
+#define CARD_LED_ADDRESS            0xD01300E4     /* maybe also 0xD01301A4 */
+#define LEDON                       0xD0002
+#define LEDOFF                      0xC0003
