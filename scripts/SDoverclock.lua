3c0ed596d2f74b69e1a2c8d7b05b4dbf3ea9b016
danne <dudek53@gmail.com>
2019-01-15 18:51:25 +0100

    lua script change

--- scripts/SDoverclock.lua
+++ scripts/SDoverclock.lua
@@ -17,8 +17,11 @@ if menu.get("Debug", "SD overclock", "MAY CAUSE DATA LOSS") == "MAY CAUSE DATA L
     menu.set("Debug", "SD overclock", "ON")
     msleep(1000)
     display.notify_box("patching...")
-    msleep(2000)
-    menu.set("Movie", "RAW video", "ON")
+    msleep(3000)
+    menu.select("Movie", "RAW video")
+    menu.open()     -- open ML menu
+    key.press(KEY.SET)
+    menu.close()
     display.notify_box("done!")
     msleep(1000)
   end
