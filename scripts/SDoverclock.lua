752e2e505f8ffce5ede793d6942681292ce28561
danne <dudek53@gmail.com>
2019-01-17 07:56:06 +0100

    lua changes and skip raw_twk for now. Interferes with sd_uhs module

--- scripts/SDoverclock.lua
+++ scripts/SDoverclock.lua
@@ -1,43 +1,39 @@
 -- enable SD overclocking
 
   console.hide()
-  menu.close()
+  msleep(200) -- let´s start off with a nap
 if menu.get("Debug", "SD overclock", "MAY CAUSE DATA LOSS") == "MAY CAUSE DATA LOSS" then
   if menu.get("Movie", "RAW video", "") == "OFF" then
       if camera.model_short == "EOSM" then
         if menu.get("Display", "Clear overlays", "") ~= "HalfShutter" then
             menu.set("Display", "Clear overlays", "HalfShutter")
            display.notify_box("enabling GD Halfshutter")
-          msleep(2000) 
+          msleep(500) 
         end
       end
- display.notify_box("enabling of SD overclocking")
+ display.notify_box("SD overclocking, please wait...")
     menu.set("Debug", "SD overclock", "ON")
-    msleep(1000)
- display.notify_box("patching...")
-    msleep(2000)
- display.notify_box("done!")
+    msleep(3000)
+ display.notify_box("patched")
     msleep(1000)
  else
     menu.set("Movie", "RAW video", "OFF")
       if camera.model_short == "EOSM" then
         if menu.get("Display", "Clear overlays", "") ~= "HalfShutter" then
             menu.set("Display", "Clear overlays", "HalfShutter")
+             menu.set("RAW video", "Preview", "Framing")
            display.notify_box("enabling GD Halfshutter")
-          msleep(2000) 
+          msleep(500) 
         end
       end
- display.notify_box("enabling of SD overclocking")
-   menu.set("Debug", "SD overclock", "ON")
-    msleep(1000)
- display.notify_box("patching...")
-    msleep(2000)
+ display.notify_box("SD overclocking, please wait...")
+    menu.set("Debug", "SD overclock", "ON")
+    msleep(3000)
     menu.select("Movie", "RAW video")
     menu.open()     -- open ML menu
     key.press(KEY.SET)
     menu.close()
- display.notify_box("done!")
-    msleep(1000)
+ display.notify_box("patched")
   end
  else
  display.notify_box("Please enable sd_uhs.mo")
@@ -46,4 +42,3 @@ if menu.get("Debug", "SD overclock", "MAY CAUSE DATA LOSS") == "MAY CAUSE DATA L
     msleep(2000)
     return
 end
-
