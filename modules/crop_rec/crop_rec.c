302e75999c9a42327f17b32bd7faa666f0ab410b
danne <dudek53@gmail.com>
2019-01-30 18:53:14 +0100

    last today

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -2795,15 +2795,15 @@ static inline uint32_t reg_override_3x3_46_48fps_eosm(uint32_t reg, uint32_t old
     switch (reg)
     {
         	case 0xC0F06804: return 0x3ef01d4 + reg_6804_width + (reg_6804_height << 16); 	
-        	case 0xC0F0713c: return 0x3e1+ reg_713c; 
-		case 0xC0F07150: return 0x3dc+ reg_7150; 
+        	case 0xC0F0713c: return 0x3eb+ reg_713c; 
+		case 0xC0F07150: return 0x3e6+ reg_7150;
 
 	     /* 46 fps */
-      	     	case 0xC0F06014: return 0x524+ reg_6014;
+      	     	case 0xC0F06014: return 0x539+ reg_6014;
  
-		case 0xC0F0600c: return 0x20f020f + reg_6008 + (reg_6008 << 16);
-		case 0xC0F06008: return 0x20f020f + reg_6008 + (reg_6008 << 16); 
-		case 0xC0F06010: return 0x20f + reg_6008;
+		case 0xC0F0600c: return 0x2070207 + reg_6008 + (reg_6008 << 16);
+		case 0xC0F06008: return 0x2070207 + reg_6008 + (reg_6008 << 16);
+		case 0xC0F06010: return 0x207 + reg_6008;
 
 		case 0xC0F06824: return 0x206;
 		case 0xC0F06828: return 0x206;
