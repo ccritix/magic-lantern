086d11f9a2a54e547cd25d9887684e46a1621a42
danne <dudek53@gmail.com>
2019-01-30 11:35:06 +0100

    most stuff works uncorrupted

--- src/raw.c
+++ src/raw.c
@@ -1721,7 +1721,9 @@ static int autodetect_black_level(int* black_mean, int* black_stdev_x100)
     int mean2 = 0;
     int stdev2 = 0;
         
-    if ((raw_info.active_area.x1 > 50) && (shamem_read(0xC0f0b13c) != 0xd)) /* use the left black bar for black calibration, shamem exception 1x3 binning crop rec.c */
+    if ((raw_info.active_area.x1 > 50) && (shamem_read(0xC0f0b13c) != 0xd) && 
+(shamem_read(0xC0f0b13c) != 0xe) && (shamem_read(0xC0f0b13c) != 0xd) && 
+(shamem_read(0xC0f0b13c) != 0xb)) /* use the left black bar for black calibration, shamem exception anamorphic and high speed frame rates crop rec.c */
     {
         autodetect_black_level_calc(
             16, raw_info.active_area.x1 - 16,
