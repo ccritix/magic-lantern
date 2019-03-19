a1e7131046c4be1f224e0f49fa7661f075a4c623
Unknown <alex@thinkpad>
2019-03-19 14:27:34 +0100

    exit_play_qr_menu_mode: wait until mode switch is finished (fixes GUI mode switching test in api_test.lua on EOS M, maybe others)

--- src/gui-common.c
+++ src/gui-common.c
@@ -791,14 +791,14 @@ void exit_play_qr_menu_mode()
         wait_lv_frames(1);
     }
 
-    /* wait for any remaining GUI stuff to settle */
-    for (int i = 0; i < 10 && !display_idle(); i++)
+    /* wait for display to come up, up to 1 second */
+    for (int i = 0; i < 10 && !DISPLAY_IS_ON; i++)
     {
         msleep(100);
     }
 
-    /* also wait for display to come up, up to 1 second */
-    for (int i = 0; i < 10 && !DISPLAY_IS_ON; i++)
+    /* wait for any remaining GUI stuff to settle, up to 2 seconds */
+    for (int i = 0; i < 20 && !display_idle(); i++)
     {
         msleep(100);
     }
