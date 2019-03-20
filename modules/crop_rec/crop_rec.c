c2ada234ebbac969e1e62273ba818d16bafdcef3
danne <dudek53@gmail.com>
2019-03-20 09:13:46 +0100

    iso check fix

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -11,7 +11,7 @@
 #include <raw.h>
 #include <fps.h>
 #include <shoot.h>
-
+#include <lens.h>
 #include "crop-mode-hack.h"
 
 #undef CROP_DEBUG
@@ -1785,12 +1785,13 @@ if ((RECORDING && is_EOSM) || (is_100D || is_5D3))
 if (RECORDING && bitdepth != 0x0)
 {
 /* correcting black level a bit. Compensating greenish tint. Only affects preview, not recordings */
-
-/* Disable for now. Doesn´t work with iso 100
+        if (lens_info.raw_iso != 100)
+        {
 	EngDrvOutLV(0xc0f37aec, 0x73cd + reg_bl);
 	EngDrvOutLV(0xc0f37af8, 0x73cd + reg_bl);
 	EngDrvOutLV(0xc0f37b04, 0x73cd + reg_bl); 
-	EngDrvOutLV(0xc0f37ae0, 0x73cd + reg_bl); */
+	EngDrvOutLV(0xc0f37ae0, 0x73cd + reg_bl); 
+	}
 }
 
 if (!RECORDING && is_EOSM)
