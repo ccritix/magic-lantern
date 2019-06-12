c9ebfc12e492c411f06453b761f041e7e58c43a1
danne <dudek53@gmail.com>
2019-06-12 21:36:59 +0200

    crop_rec.c,mlv_lite.c:(100D height expansion 4k when timelapsing)

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -3060,7 +3060,7 @@ static inline uint32_t reg_override_4K_100d(uint32_t reg, uint32_t old_val)
     {
         /* raw resolution (end line/column) */
         /* X: (3072+140)/8 + 0x17, adjusted for 3072 in raw_rec */
-        case 0xC0F06804: return 0x90d0421 + reg_6804_width + (reg_6804_height << 16); // 4096x2560  x5 Mode;
+        case 0xC0F06804: return (timelapse != 0x0 && ratios != 0x3) ? 0xa1b0421: 0x90d0421 + reg_6804_width + (reg_6804_height << 16); // 4096x2560  x5 Mode;
 
         case 0xC0F06824: return 0x4ca;
         case 0xC0F06828: return 0x4ca;
@@ -3078,8 +3078,8 @@ static inline uint32_t reg_override_4K_100d(uint32_t reg, uint32_t old_val)
 				(RECORDING && timelapse == 0x5) ? 0x1bfe:
 				(RECORDING && timelapse == 0x6) ? 0x1665: 0xfff + reg_6014;
 
-        case 0xC0F0713c: return 0x90d + reg_713c;
-        case 0xC0F07150: return 0x8f9 + reg_7150;
+        case 0xC0F0713c: return (timelapse != 0x0 && ratios != 0x3) ? 0xa55 :0x90d + reg_713c;
+        case 0xC0F07150: return (timelapse != 0x0 && ratios != 0x3) ? 0xa00 :0x8f9 + reg_7150;
 
     }
 
--- modules/mlv_lite/mlv_lite.c
+++ modules/mlv_lite/mlv_lite.c
@@ -2103,7 +2103,10 @@ else
              if (shamem_read(0xC0F06804) == 0x88501c2) *(volatile uint32_t*)0xC0F06804 = 0x88501d4; 
 /* 100D 3k, 4k. Registry from crop_rec.c / Needs more tests, not working atm. */
              if (shamem_read(0xC0F06804) == 0x5b90319) *(volatile uint32_t*)0xC0F06804 = 0x45802a1;
-             if (shamem_read(0xC0F06804) == 0x90d0421) *(volatile uint32_t*)0xC0F06804 = 0x45802a1; 
+             if (shamem_read(0xC0F06804) == 0x90d0421) *(volatile uint32_t*)0xC0F06804 = 0x45802a1;
+             if (shamem_read(0xC0F06804) == 0xa1b0421) *(volatile uint32_t*)0xC0F06804 = 0x45802a1; 
+
+
 
      }
      if (!get_halfshutter_pressed())
@@ -2116,7 +2119,8 @@ else
              if (shamem_read(0xC0F06804) == 0xa1c0412) *(volatile uint32_t*)0xC0F06804 = 0xa1c0412; raw_set_preview_rect(skip_x, skip_y, res_x, res_y, 1); raw_force_aspect_ratio(0, 0);
 /* 100D reset regs / Needs more tests, not working atm.	*/
              if (shamem_read(0xC0F06804) == 0x5b90319) *(volatile uint32_t*)0xC0F06804 = 0x5b90319; raw_set_preview_rect(skip_x, skip_y, res_x, res_y, 1); raw_force_aspect_ratio(0, 0);
-             if (shamem_read(0xC0F06804) == 0x90d0421) *(volatile uint32_t*)0xC0F06804 = 0x90d0421; raw_set_preview_rect(skip_x, skip_y, res_x, res_y, 1); raw_force_aspect_ratio(0, 0); 
+             if (shamem_read(0xC0F06804) == 0x90d0421) *(volatile uint32_t*)0xC0F06804 = 0x90d0421; raw_set_preview_rect(skip_x, skip_y, res_x, res_y, 1); raw_force_aspect_ratio(0, 0);
+             if (shamem_read(0xC0F06804) == 0xa1b0421) *(volatile uint32_t*)0xC0F06804 = 0xa1b0421; raw_set_preview_rect(skip_x, skip_y, res_x, res_y, 1); raw_force_aspect_ratio(0, 0); 
      }
   }
     
