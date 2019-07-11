2b6c6d3c16bd103d0a65cfdb6b75c2a1f223caf8
danne <dudek53@gmail.com>
2019-07-11 15:54:45 +0200

    crop_rec.c:(Good info)

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -5204,7 +5204,7 @@ static struct menu_entry crop_rec_menu[] =
                 .max    = 1,
                 .choices = CHOICES("OFF", "x10toggle"),
                 .help   = "Toggle in and out of x10crop by holding halfshutter(all presets)",
-                .help2   = "x10 zoom aid.\n"                          
+                .help2   = "x10 zoom aid. Manual focus.\n"                          
             },
             {
                 .name   = "set 25fps",
