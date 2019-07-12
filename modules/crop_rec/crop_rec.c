996df620df13de141a8895c8becb5f94146e3e87
danne <dudek53@gmail.com>
2019-07-12 09:20:29 +0200

    crop_rec.c:(100D x3crop mode preset centering)

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -1438,8 +1438,8 @@ static void FAST cmos_hook(uint32_t* regs, uint32_t* stack, uint32_t pc)
 	        cmos_new[8] = 0x400; 
                 break;
 			case CROP_PRESET_3xcropmode_100D:
-                cmos_new[5] = 0x300;            /* vertical (first|last) */
-                cmos_new[7] = 0x200;
+                cmos_new[5] = 0x380;            /* vertical (first|last) */
+                cmos_new[7] = 0xb;
                 break;
 
 			case CROP_PRESET_2K_100D:
