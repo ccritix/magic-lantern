58dc44876f67823bb6b9b08220d5edc1c959d96c
danne <dudek53@gmail.com>
2019-08-04 16:46:44 +0200

    crop_rec.c:(turning off Switch menu for other cameras but eosm. Jip-Hop fix)

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -5635,7 +5635,7 @@ static MENU_UPDATE_FUNC(switch_menu_toggle_update)
 
 static MENU_UPDATE_FUNC(x3crop_entry_update)
 {
-    if(switch_menu_enabled && is_EOSM){
+    if(switch_menu_enabled){
         MENU_SET_WARNING(MENU_WARN_NOT_WORKING, "This now affects \"Toggle mv1080\" in Switch menu.");
     }
 }
@@ -7569,6 +7569,11 @@ static unsigned int raw_info_update_cbr(unsigned int unused)
 
 static unsigned int crop_rec_init()
 {
+
+    if(!is_EOSM){
+        switch_menu_enabled = 0;
+    }
+
     is_digic4 = is_camera("DIGIC", "4");
     is_digic5 = is_camera("DIGIC", "5");
     
