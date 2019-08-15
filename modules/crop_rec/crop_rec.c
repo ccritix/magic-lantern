0c0cd6dd6e019a8cc2720589bfcd64698dd1d1de
danne <dudek53@gmail.com>
2019-08-15 09:51:31 +0200

    crop_rec.c:(EOSM updating ml menu iso)

--- modules/crop_rec/crop_rec.c
+++ modules/crop_rec/crop_rec.c
@@ -5780,7 +5780,7 @@ static struct menu_entry crop_rec_menu[] =
 
 static unsigned int crop_rec_keypress_cbr(unsigned int key)
 {
-    if (key == MODULE_KEY_INFO && gain_buttons)
+    if ((key == MODULE_KEY_INFO && gain_buttons) || gui_menu_shown())
     {
         gain = 1;
     }
